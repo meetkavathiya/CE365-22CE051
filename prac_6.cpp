5a53945de8aaceaf96be0a75abd52bb801d1d776
meetkavathiya <meetpatel76413333@gmail.com>
2025-02-27 14:00:52 +0530

    prac-6 & 7

---
+++ prac_6.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string input;
+int index = 0;
+
+bool S();
+bool L();
+bool L_();
+
+bool S()
+{
+    if(input[index]=='(')
+    {
+        index++;
+       bool a= L();
+       if(a && input[index]==')' )
+       {
+        index++;
+        return true;
+       }
+       else{
+        return false;
+       }
+       
+
+    }
+    else if(input[index]=='a')
+    {
+        index++;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+    
+}
+
+bool L()
+{
+
+    if(S() && L_())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+bool L_()
+{
+    if(input[index]==','  )
+    {
+        index++;
+
+        if(S() && L_())
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else 
+    {
+        return true;
+    }
+}
+
+
+bool validateString()
+{
+     index = 0;
+    if (S() && index == input.length()) {
+        return true;
+    }
+    return false;
+
+}
+
+
+int main() {
+    cout << "Enter a string: ";
+    cin >> input;
+
+    if (validateString()) {
+        cout << "Valid string" << endl;
+    } else {
+        cout << "Invalid string" << endl;
+    }
+
+    return 0;
+}
